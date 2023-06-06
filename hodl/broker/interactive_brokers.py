ec1f8804fe7859ecde19e50d82869c3efca0645f
songwei <songwei@songwei.io>
2023-06-06 23:47:13 +0800

    盈透证券现金、持仓、下撤查单

--- hodl/broker/interactive_brokers.py
+++ hodl/broker/interactive_brokers.py
@@ -7,6 +7,7 @@ from urllib.parse import urljoin
 from hodl.broker.base import *
 from hodl.exception_tools import *
 from hodl.quote import *
+from hodl.state import *
 from hodl.tools import *
 
 
@@ -17,6 +18,10 @@ class InteractiveBrokers(BrokerApiBase):
     BROKER_NAME = 'interactiveBrokers'
     BROKER_DISPLAY = '盈透证券'
 
+    PLUGIN_BUCKET = LeakyBucket(60)
+    ACCOUNT_BUCKET = LeakyBucket(60)
+    ORDER_BUCKET = LeakyBucket(120)
+
     @classmethod
     def http_request(
             cls,
@@ -24,6 +29,7 @@ class InteractiveBrokers(BrokerApiBase):
             path: str,
             method: str,
             params: dict | None = None,
+            json: dict | None = None,
             session: requests.Session = None,
     ) -> dict:
         timeout = ib_config.get('timeout', 16)
@@ -33,6 +39,7 @@ class InteractiveBrokers(BrokerApiBase):
             method=method,
             url=url,
             params=params,
+            json=json,
             verify=False,
             timeout=timeout,
         )
@@ -43,32 +50,37 @@ class InteractiveBrokers(BrokerApiBase):
         resp.raise_for_status()
         return resp.json()
 
+    def account_id(self) -> str:
+        return self.broker_config.get('account_id')
+
     @track_api
     def detect_plug_in(self):
-        try:
-            d = self.http_request(
-                ib_config=self.broker_config,
-                path='/v1/api/tickle',
-                method='POST',
-                session=self.http_session,
-            )
-            authenticated = d.get('iserver', dict()).get('authStatus', dict()).get('authenticated', False)
-            return authenticated
-        except Exception as e:
-            return False
+        with self.PLUGIN_BUCKET:
+            try:
+                d = self.http_request(
+                    ib_config=self.broker_config,
+                    path='/v1/api/tickle',
+                    method='POST',
+                    session=self.http_session,
+                )
+                authenticated = d.get('iserver', dict()).get('authStatus', dict()).get('authenticated', False)
+                return authenticated
+            except Exception as e:
+                return False
 
     @track_api
     def query_cash(self) -> float:
-        account_id = self.broker_config.get('account_id')
-        d = self.http_request(
-            ib_config=self.broker_config,
-            path=f'/v1/api/portfolio/{account_id}/summary',
-            method='GET',
-            session=self.http_session,
-        )
-        available_funds = d.get('availablefunds', dict())
-        amount = available_funds.get('amount', 0.0)
-        return amount
+        with self.ACCOUNT_BUCKET:
+            account_id = self.broker_config.get('account_id')
+            d = self.http_request(
+                ib_config=self.broker_config,
+                path=f'/v1/api/portfolio/{account_id}/summary',
+                method='GET',
+                session=self.http_session,
+            )
+            available_funds = d.get('availablefunds', dict())
+            amount = available_funds.get('amount', 0.0)
+            return amount
 
     def query_conid(self):
         symbol = self.symbol
@@ -100,6 +112,117 @@ class InteractiveBrokers(BrokerApiBase):
             else:
                 raise IbkrConidMissingError(f'{symbol}找不到对应的conid')
         return conid
+
+    @track_api
+    def query_chips(self) -> int:
+        """
+        返回 self.symbol 获取实际持仓数量。
+        """
+        with self.ACCOUNT_BUCKET:
+            account_id = self.broker_config.get('account_id')
+            conid = self.query_conid()
+            d = self.http_request(
+                ib_config=self.broker_config,
+                path=f'/v1/api/portfolio/{account_id}/position/{conid}',
+                method='GET',
+                session=self.http_session,
+            )
+            if d:
+                position = d[0].get('position', 0)
+                position = int(position)
+                return position
+            else:
+                return 0
+
+    @track_api
+    def place_order(self, order: Order):
+        with self.ORDER_BUCKET:
+            account_id = self.account_id()
+            conid = self.query_conid()
+            args = {
+                'orders': [
+                    {
+                        'conid': conid,
+                        'orderType': 'MKT' if order.limit_price is None else 'LMT',
+                        'price': order.limit_price,
+                        'side': order.direction,
+                        'tif': 'DAY',
+                        'quantity': order.qty,
+                    },
+                ],
+            }
+            d = self.http_request(
+                ib_config=self.broker_config,
+                path=f'/v1/api/iserver/account/{account_id}/orders',
+                method='POST',
+                json=args,
+                session=self.http_session,
+            )
+            if not d:
+                raise TradingError(f'盈透证券下单接口返回了空数据')
+            order_resp: dict = d[0]
+            reply_id = order_resp.get('id')
+            message = order_resp.get('message')
+            is_suppressed = order_resp.get('isSuppressed', True)
+            if message and not is_suppressed:
+                args = {
+                    'confirmed': True,
+                }
+                d = self.http_request(
+                    ib_config=self.broker_config,
+                    path=f'/v1/api/iserver/reply/{reply_id}',
+                    method='POST',
+                    json=args,
+                    session=self.http_session,
+                )
+                if not d:
+                    raise TradingError(f'盈透证券确认下单接口返回了空数据')
+                reply_resp: dict = d[0]
+                order_id = reply_resp.get('order_id')
+                error = reply_resp.get('error')
+                if order_id:
+                    order.order_id = order_id
+                else:
+                    order.error_reason = error
+                    raise TradingError(f'盈透证券确认下单接口返回失败原因:{error}')
+            else:
+                raise TradingError(f'盈透证券确认下单接口没有给出确认信息')
+
+    @track_api
+    def cancel_order(self, order: Order):
+        with self.ORDER_BUCKET:
+            account_id = self.account_id()
+            d: dict = self.http_request(
+                ib_config=self.broker_config,
+                path=f'/v1/api/iserver/account/{account_id}/order/{order.order_id}',
+                method='DELETE',
+                session=self.http_session,
+            )
+            if not d:
+                raise TradingError(f'盈透证券确认撤单接口返回了空数据')
+
+    @track_api
+    def refresh_order(self, order: Order):
+        with self.ORDER_BUCKET:
+            d: dict = self.http_request(
+                ib_config=self.broker_config,
+                path=f'/v1/api/iserver/account/order/status/{order.order_id}',
+                method='GET',
+                session=self.http_session,
+            )
+            order_status = d.get('order_status', '')
+            qty = int(float(d.get('size')))
+            filled_qty = int(float(d.get('cum_fill')))
+            avg_fill_price = float(d.get('average_price', 0.0))
+            self.modify_order_fields(
+                order=order,
+                qty=qty,
+                filled_qty=filled_qty,
+                avg_fill_price=avg_fill_price,
+                trade_timestamp=None,
+                reason='',
+                is_cancelled=order_status=='Cancelled',
+            )
 
     @track_api
     def fetch_quote(self) -> Quote:
