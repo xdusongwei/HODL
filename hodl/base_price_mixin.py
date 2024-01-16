3d8ee1813ba966e6c07d7c1b9179284233f3d074
songwei <songwei@songwei.io>
2024-01-16 17:58:49 +0800

    重构技术分析

--- hodl/base_price_mixin.py
+++ hodl/base_price_mixin.py
@@ -1,11 +1,11 @@
 from abc import ABC
-from collections import defaultdict
 from dataclasses import dataclass, field, asdict
 import numpy
 from hodl.quote import *
 from hodl.store_base import *
 from hodl.storage import *
 from hodl.tools import *
+from hodl.ta import *
 
 
 @dataclass
@@ -172,6 +172,7 @@ class BasePriceMixin(StoreBase, ABC):
     def _rsi_tp_check(self):
         state = self.state
         store_config = self.store_config
+        ta = TaTools(cfg=self.store_config, db=self.db)
 
         state.ta_tumble_protect_rsi_current = None
         if state.ta_tumble_protect_rsi is not None:
@@ -182,26 +183,26 @@ class BasePriceMixin(StoreBase, ABC):
             assert state.ta_tumble_protect_rsi_period
             rsi_period = state.ta_tumble_protect_rsi_period
             rsi_day = state.ta_tumble_protect_rsi_day
-            points = self._query_day_avg(days=rsi_period * 20, asc=True)
-            rsi_points = self._rsi(points, period=rsi_period)
+            candles = ta.query_days(days=rsi_period * 20, asc=True)
+            rsi_list = ta.rsi(candles, period=rsi_period)
             if store_config.tumble_protect_rsi_unlock_limit:
                 # 可以随时更新上限阈值
                 state.ta_tumble_protect_rsi = store_config.tumble_protect_rsi_unlock_limit
-            if any(point for point in rsi_points if point[0] > rsi_day and point[1] >= unlock_limit):
+            if any(point for point in rsi_list if point.time > rsi_day and point.v >= unlock_limit):
                 state.ta_tumble_protect_rsi = None
                 state.ta_tumble_protect_rsi_day = None
                 state.ta_tumble_protect_rsi_period = None
-            if rsi_points:
-                state.ta_tumble_protect_rsi_current = rsi_points[-1][1]
+            if rsi_list:
+                state.ta_tumble_protect_rsi_current = rsi_list[-1].v
         elif store_config.tumble_protect_rsi and state.plan.cleanable:
             # RSI TP unlocked
             # 没有有效计划时, 再判断触发RSI的逻辑, 防止计划执行中触发RSI保护
             rsi_period = store_config.tumble_protect_rsi_period
-            points = self._query_day_avg(days=rsi_period * 20, asc=True)
-            rsi_points = self._rsi(points, period=store_config.tumble_protect_rsi_period)
-            if rsi_points:
-                rsi_day = rsi_points[-1][0]
-                rsi = rsi_points[-1][1]
+            candles = ta.query_days(days=rsi_period * 20, asc=True)
+            rsi_list = ta.rsi(candles, period=store_config.tumble_protect_rsi_period)
+            if rsi_list:
+                rsi_day = rsi_list[-1].time
+                rsi = rsi_list[-1].v
                 lock_limit = store_config.tumble_protect_rsi_lock_limit
                 unlock_limit = store_config.tumble_protect_rsi_unlock_limit
                 assert lock_limit < unlock_limit
@@ -213,62 +214,32 @@ class BasePriceMixin(StoreBase, ABC):
                 elif store_config.tumble_protect_rsi_warning_limit is not None:
                     if rsi <= store_config.tumble_protect_rsi_warning_limit:
                         self._set_bp_function('median')
-            if rsi_points:
-                state.ta_tumble_protect_rsi_current = rsi_points[-1][1]
+            if rsi_list:
+                state.ta_tumble_protect_rsi_current = rsi_list[-1].v
         else:
             state.ta_tumble_protect_rsi = None
             state.ta_tumble_protect_rsi_day = None
             state.ta_tumble_protect_rsi_period = None
 
-    def _query_history(self, days: int, day_low=True, asc=False):
-        db = self.db
-        if not db:
-            return list()
-        store_config = self.store_config
-        end_day = TimeTools.us_time_now()
-        begin_day = TimeTools.timedelta(end_day, days=-days)
-        args = dict(
-            con=db.conn,
-            broker=store_config.broker,
-            region=store_config.region,
-            symbol=store_config.symbol,
-            begin_day=int(TimeTools.date_to_ymd(begin_day, join=False)),
-            end_day=int(TimeTools.date_to_ymd(end_day, join=False)),
-        )
-        if day_low:
-            history = QuoteLowHistoryRow.query_by_symbol(**args)
-        else:
-            history = QuoteHighHistoryRow.query_by_symbol(**args)
-        if asc:
-            history = history[::-1]
-        return history
-
-    def _query_day_avg(self, days: int, asc=True):
-        low_his: list[QuoteLowHistoryRow] = self._query_history(days=days, day_low=True, asc=True)
-        high_his: list[QuoteHighHistoryRow] = self._query_history(days=days, day_low=False, asc=True)
-        d: dict[int, list[float]] = defaultdict(list)
-        for item in low_his:
-            d[item.day].append(item.low_price)
-        for item in high_his:
-            d[item.day].append(item.high_price)
-        result = [(day, sum(nums) / len(nums), ) for day, nums in d.items() if len(nums) > 1]
-        result = sorted(result, key=lambda i: i[0], reverse=not asc)
-        return result
-
     def _detect_lowest_days(self) -> bool:
         store_config = self.store_config
+        ta = TaTools(cfg=store_config, db=self.db)
         tumble_protect_day_range = store_config.tumble_protect_day_range
         tumble_protect_sample_range = store_config.tumble_protect_sample_range
-        history: list[QuoteLowHistoryRow] = self._query_history(days=tumble_protect_sample_range)
+        history = ta.query_days(days=tumble_protect_sample_range)
         if len(history) <= tumble_protect_day_range:
             return False
+        history_low = min(day.low_price for day in history) * 1.01
         recent = history[:tumble_protect_day_range]
-        return min(day.low_price for day in history) * 1.01 >= min(day.low_price for day in recent)
+        recent_low = min(day.low_price for day in recent)
+        return history_low >= recent_low
 
     def _tumble_protect_alert_price(self):
-        ma5_days: list[QuoteHighHistoryRow] = self._query_history(days=5, day_low=False)
+        ta = TaTools(cfg=self.store_config, db=self.db)
+        ta.query_days(days=5)
+        ma5_days = ta.query_days(days=5)
         ma5_price_list = [day.high_price for day in ma5_days]
-        ma10_days: list[QuoteHighHistoryRow] = self._query_history(days=10, day_low=False)
+        ma10_days = ta.query_days(days=10)
         ma10_price_list = [day.high_price for day in ma10_days]
         if not ma5_price_list or not ma10_price_list:
             return None
@@ -281,64 +252,6 @@ class BasePriceMixin(StoreBase, ABC):
         assert ma5 > 0
         assert ma10 > 0
         return max(ma5, ma10)
-
-    @classmethod
-    def _rsi(cls, points: list[tuple[int, float]], period: int = 6) -> list[tuple[int, float]]:
-        init_value = 0.5
-        rsi_points: list[tuple[int, float]] = list()
-        if len(points) < period:
-            for day, _ in points:
-                rsi_points.append((day, init_value, ))
-            return cls._transform_rsi_rate(rsi_points)
-        t_max = cls._rsi_t(points, period=period, t_max=True)
-        t_abs = cls._rsi_t(points, period=period, t_max=False)
-        assert len(t_max) == len(t_abs)
-        for idx in range(len(t_max)):
-            max_item = t_max[idx]
-            abs_item = t_abs[idx]
-            max_value = max_item[1]
-            abs_value = abs_item[1]
-            assert max_item[0] == abs_item[0]
-            if abs_value:
-                rsi_point = (max_item[0], max_value / abs_value,)
-                rsi_points.append(rsi_point)
-            else:
-                rsi_point = (max_item[0], init_value,)
-                rsi_points.append(rsi_point)
-        return cls._transform_rsi_rate(rsi_points)
-
-    @classmethod
-    def _rsi_t(cls, points: list[tuple[int, float]], period: int = 6, t_max=True) -> list[tuple[int, float]]:
-        sma_points: list[tuple[int, float]] = list()
-        # 返回一个由后一日期，和后一日价格减去前一日价格的二元组列表
-        diff_points = [(n[0], n[1] - p[1],) for p, n in zip(points[:-1], points[1:])]
-        for idx in range(len(diff_points)):
-            day = diff_points[idx][0]
-            diff = diff_points[idx][1]
-            if not diff:
-                if idx:
-                    last_sma_point = sma_points[-1]
-                    sma_point = (day, last_sma_point[1],)
-                    sma_points.append(sma_point)
-                else:
-                    sma_point = (day, 0,)
-                    sma_points.append(sma_point)
-                continue
-            if t_max:
-                t = max(0.0, diff)
-            else:
-                t = abs(diff)
-            s = t
-            if idx:
-                last_sma_point = sma_points[-1]
-                s += last_sma_point[1] * (period - 1)
-            sma_point = (day, s / period,)
-            sma_points.append(sma_point)
-        return sma_points
-
-    @classmethod
-    def _transform_rsi_rate(cls, sma_points: list[tuple[int, float]]) -> list[tuple[int, float]]:
-        return [(day, FormatTool.adjust_precision(rate * 100, precision=2), ) for day, rate in sma_points]
 
 
 __all__ = ['BasePriceMixin', ]
---
+++ hodl/ta/ta_tools.py
@@ -0,0 +1,140 @@
+from collections import defaultdict
+from dataclasses import dataclass, field
+from hodl.storage import *
+from hodl.tools import *
+
+
+@dataclass
+class Candle:
+    time: int
+    high_price: float = field(default=None)
+    low_price: float = field(default=None)
+
+    @property
+    def avg_price(self) -> None | float:
+        if not isinstance(self.high_price, float) or not isinstance(self.low_price, float):
+            return None
+        if self.high_price <= 0.0 or self.low_price <= 0.0:
+            return None
+        return (self.high_price + self.low_price) / 2.0
+
+
+@dataclass
+class RSI:
+    time: int
+    period: int
+    v: float = field(default=None)
+
+
+class TaTools:
+    def __init__(self, cfg: StoreConfig, db: LocalDb):
+        self.db = db
+        self.cfg = cfg
+
+    def _query_history(self, days: int, day_low=True, asc=False):
+        db = self.db
+        if not db:
+            return list()
+        store_config = self.cfg
+        end_day = TimeTools.us_time_now()
+        begin_day = TimeTools.timedelta(end_day, days=-days)
+        args = dict(
+            con=db.conn,
+            broker=store_config.broker,
+            region=store_config.region,
+            symbol=store_config.symbol,
+            begin_day=int(TimeTools.date_to_ymd(begin_day, join=False)),
+            end_day=int(TimeTools.date_to_ymd(end_day, join=False)),
+        )
+        if day_low:
+            history = QuoteLowHistoryRow.query_by_symbol(**args)
+        else:
+            history = QuoteHighHistoryRow.query_by_symbol(**args)
+        if asc:
+            history = history[::-1]
+        return history
+
+    def query_days(self, days: int, asc=True) -> list[Candle]:
+        low_his: list[QuoteLowHistoryRow] = self._query_history(days=days, day_low=True, asc=True)
+        high_his: list[QuoteHighHistoryRow] = self._query_history(days=days, day_low=False, asc=True)
+        d: dict[int, list[float]] = defaultdict(list)
+        for item in low_his:
+            d[item.day].append(item.low_price)
+        for item in high_his:
+            d[item.day].append(item.high_price)
+        result = [
+            Candle(time=day, high_price=max(nums), low_price=min(nums))
+            for day, nums in d.items() if len(nums) > 1
+        ]
+        result = sorted(result, key=lambda i: i.time, reverse=not asc)
+        return result
+
+    @classmethod
+    def rsi(cls, candles: list[Candle], period: int = 6) -> list[RSI]:
+        init_value = 0.5
+        rsi_points: list[RSI] = list()
+        if len(candles) < period:
+            for day, _ in candles:
+                rsi_points.append(RSI(time=day, period=period, v=init_value, ))
+            return cls._transform_rsi_rate(rsi_points)
+        t_max = cls._rsi_t(candles, period=period, t_max=True)
+        t_abs = cls._rsi_t(candles, period=period, t_max=False)
+        assert len(t_max) == len(t_abs)
+        for idx in range(len(t_max)):
+            max_item = t_max[idx]
+            abs_item = t_abs[idx]
+            max_value = max_item[1]
+            abs_value = abs_item[1]
+            t = max_item[0]
+            assert max_item[0] == abs_item[0]
+            if abs_value:
+                rsi_points.append(RSI(time=t, period=period, v=max_value / abs_value))
+            else:
+                rsi_points.append(RSI(time=t, period=period, v=init_value))
+        return cls._transform_rsi_rate(rsi_points)
+
+    @classmethod
+    def _rsi_t(cls, points: list[Candle], period: int = 6, t_max=True) -> list[tuple[int, float]]:
+        sma_points: list[tuple[int, float]] = list()
+        points = points.copy()
+        points.sort(key=lambda i: i.time)
+        # 返回一个由后一日期，和后一日价格减去前一日价格的二元组列表
+        lp = points[:-1]
+        ln = points[1:]
+        diff_points: list[tuple[int, float]] = [(n.time, n.avg_price - p.avg_price,) for p, n in zip(lp, ln)]
+
+        for idx in range(len(diff_points)):
+            day, diff = diff_points[idx][0], diff_points[idx][1]
+            if not diff:
+                if idx:
+                    last_sma_point = sma_points[-1]
+                    sma_point = (day, last_sma_point[1],)
+                    sma_points.append(sma_point)
+                else:
+                    sma_point = (day, 0,)
+                    sma_points.append(sma_point)
+                continue
+            if t_max:
+                t = max(0.0, diff)
+            else:
+                t = abs(diff)
+            s = t
+            if idx:
+                last_sma_point = sma_points[-1]
+                s += last_sma_point[1] * (period - 1)
+            sma_point = (day, s / period,)
+            sma_points.append(sma_point)
+        return sma_points
+
+    @classmethod
+    def _transform_rsi_rate(cls, points: list[RSI]) -> list[RSI]:
+        for point in points:
+            point.v = FormatTool.adjust_precision(point.v * 100, precision=2)
+        return points
+
+
+__all__ = [
+    'Candle',
+    'RSI',
+    'TaTools',
+]
