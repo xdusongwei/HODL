f746d77eb8358fef148e21987da800d69572ee01
songwei <songwei@songwei.io>
2024-06-20 20:57:40 +0800

    fix lock

--- hodl/tools/locks.py
+++ hodl/tools/locks.py
@@ -11,18 +11,14 @@ class Filelock:
             self.lock_file = FileLock(lock_file, timeout=self.timeout)
 
     def __enter__(self):
-        if isinstance(self.lock_file, RLock):
-            self.lock_file.acquire(timeout=self.timeout)
-        elif isinstance(self.lock_file, FileLock):
-            self.lock_file.acquire(timeout=self.timeout)
+        self.lock_file.acquire(timeout=self.timeout)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if isinstance(self.lock_file, RLock):
-            self.lock_file.release()
-        elif isinstance(self.lock_file, FileLock):
-            if self.lock_file.is_locked:
-                self.lock_file.release()
+        if isinstance(self.lock_file, FileLock):
+            if not self.lock_file.is_locked:
+                return
+        self.lock_file.release()
 
 
 __all__ = ['Filelock']
