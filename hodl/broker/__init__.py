61f62edaa27c8ca998855f86b284a3d24f608a55
songwei <songwei@songwei.io>
2024-12-15 18:57:49 +0800

    移除币安的支持

--- hodl/broker/__init__.py
+++ hodl/broker/__init__.py
@@ -2,7 +2,6 @@ from hodl.broker.base import *
 from hodl.broker.tiger import *
 from hodl.broker.citics import *
 from hodl.broker.okx import *
-from hodl.broker.binance import *
 from hodl.broker.futu import *
 from hodl.broker.interactive_brokers import *
 from hodl.broker.myquant import *
